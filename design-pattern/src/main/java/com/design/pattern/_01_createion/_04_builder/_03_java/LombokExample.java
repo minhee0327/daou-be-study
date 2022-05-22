f6e0a9fe13b1f9882ddf3d170a22f25c1405b777
강민희 <queen.minhee@gmail.com>
2022-05-23 07:07:40 +0900

    Design Pattern: Prototype Pattern

---
+++ design-pattern/src/main/java/com/design/pattern/_01_createion/_04_builder/_03_java/LombokExample.java
@@ -0,0 +1,23 @@
+package com.design.pattern._01_createion._04_builder._03_java;
+
+
+import lombok.Builder;
+
+@Builder
+public class LombokExample {
+
+    private String title;
+
+    private int nights;
+
+    private int days;
+
+    public static void main(String[] args) {
+        LombokExample trip = LombokExample.builder()
+                .title("여행")
+                .nights(2)
+                .days(3)
+                .build();
+    }
+
+}
