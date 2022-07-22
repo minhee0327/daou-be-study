059e52a824d23ebf51a3026edec3d542d4a081e1
강민희 <queen.minhee@gmail.com>
2022-07-23 04:08:41 +0900

    Spring Data: 데이터 접근예외 직접만들기

--- jdbc/src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ jdbc/src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -5,6 +5,9 @@ import hello.jdbc.repository.ex.MyDBException;
 import hello.jdbc.repository.ex.MyDuplicateKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
@@ -13,8 +16,26 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Random;
 
+import static hello.jdbc.connection.ConnectionConst.*;
+
 @Slf4j
 public class ExTranslatorV1Test {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myId");
+        service.create("myId"); //같은 아이디 시도
+    }
 
     @RequiredArgsConstructor
     @Slf4j
@@ -30,14 +51,14 @@ public class ExTranslatorV1Test {
                 String retryId = generateNewId(memberId);
                 log.info("retryId={}", retryId);
                 repository.save(new Member(retryId, 0));
-            } catch (MyDBException e){
+            } catch (MyDBException e) {
                 log.info("데이터 접근 계층 예외", e);
                 throw e;
             }
         }
 
-        private String generateNewId(String memberId){
-            return memberId + new Random().nextInt();
+        private String generateNewId(String memberId) {
+            return memberId + new Random().nextInt(10000);
         }
     }
 
@@ -46,7 +67,7 @@ public class ExTranslatorV1Test {
         private final DataSource dataSource;
 
         public Member save(Member member) {
-            String sql = "insert into member(member_id, money0 values(?, ?)";
+            String sql = "insert into member(member_id, money) values(?, ?)";
             Connection con = null;
             PreparedStatement pstmt = null;
 
@@ -55,20 +76,18 @@ public class ExTranslatorV1Test {
                 pstmt = con.prepareStatement(sql);
                 pstmt.setString(1, member.getMemberId());
                 pstmt.setInt(2, member.getMoney());
-                pstmt.executeQuery();
+                pstmt.executeUpdate();
                 return member;
             } catch (SQLException e) {
                 //h2 db
                 if (e.getErrorCode() == 23505) {
                     throw new MyDuplicateKeyException(e);
                 }
+                throw new MyDBException(e);
             } finally {
                 JdbcUtils.closeStatement(pstmt);
                 JdbcUtils.closeConnection(con);
             }
-
-            return member;
         }
-
     }
 }
