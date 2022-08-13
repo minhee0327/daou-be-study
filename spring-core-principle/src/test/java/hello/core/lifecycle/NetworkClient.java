ef4134ab0ad01f5f65a0a67056eb9cb2264be6ed
강민희 <queen.minhee@gmail.com>
2022-08-13 10:55:17 +0900

    빈생성주기 콜백: 인터페이스

--- spring-core-principle/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-core-principle/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    //의존관계 주입이 끝나면 넣겠다. (싱글톤 빈, 스프링컨테이너 올라올때 의존관계 주입이 끝나고 넣어짐)
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    //빈이 종료될 때 호출된다.
+    @Override
+    public void destroy() throws Exception {
+      disconnect();
     }
 }
