bc184627bfce21b17cf36e19faebee04a332abda
강민희 <queen.minhee@gmail.com>
2023-02-25 17:53:45 +0900

    chapter05 모듈 추가 - core 모듈 사용테스트 진행 (core에 만든 serivce ch04, 05에서 사용)

--- wiki/chapter05/src/main/java/com/daou/wiki/controller/TempController.java
+++ wiki/chapter05/src/main/java/com/daou/wiki/controller/TempController.java
@@ -2,17 +2,17 @@ package com.daou.wiki.controller;
 
 import com.daou.wiki.service.HelloService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class HelloController {
+public class TempController {
 
     private final HelloService helloService;
 
-    @RequestMapping("/hello")
-    public String hello(){
+    @GetMapping("/temp")
+    public String temp(){
         return helloService.hello();
     }
 }
---
+++ wiki/core/src/main/java/com/daou/wiki/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.daou.wiki.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String hello(){
+        return "hello";
+    }
+}
