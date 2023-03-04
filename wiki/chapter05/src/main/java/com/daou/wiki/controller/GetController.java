da3c55f2f4fa73e00c5328d30d60113b5307a2db
강민희 <queen.minhee@gmail.com>
2023-03-04 13:20:10 +0900

    5.7 장 로그백 설정 테스트 진행 - 진행한 내용 토대로 회사 코드 다시 볼것..:D

--- wiki/chapter05/src/main/java/com/daou/wiki/controller/GetController.java
+++ wiki/chapter05/src/main/java/com/daou/wiki/controller/GetController.java
@@ -3,6 +3,8 @@ package com.daou.wiki.controller;
 import com.daou.wiki.dto.MemberDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -11,15 +13,19 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     //RequestMapping 에서 GET 메서드를 사용하는 방뻡
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello world";
     }
 
     //5.2.2 매개변수가 없는 GET 메서드 구현
     @GetMapping(value = "/name")
     public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Flature";
     }
 
@@ -28,6 +34,7 @@ public class GetController {
     //중괄호안의 변수명과 괄호의 변수명을 동일하게 맞추기. 맞추기 어렵다면 @PathVariable 괄호안에 변수명을 지정하기
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable 을 통해서 들어온 값: {}", variable);
         return variable;
     }
 
