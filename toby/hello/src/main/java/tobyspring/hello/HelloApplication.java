463950ac8dd63f2079fe154bcf3237c9de65bd57
강민희 <queen.minhee@gmail.com>
2023-05-23 17:15:55 +0900

    - section 4 마무리

--- toby/hello/src/main/java/tobyspring/hello/HelloApplication.java
+++ toby/hello/src/main/java/tobyspring/hello/HelloApplication.java
@@ -2,47 +2,27 @@ package tobyspring.hello;
 
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.awt.print.Pageable;
 
 @Configuration
 @ComponentScan
 public class HelloApplication {
 
     @Bean
-    public ServletWebServerFactory servletWebServerFactory(){
+    public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
 
     @Bean
-    public DispatcherServlet dispatcherServlet(){
+    public DispatcherServlet dispatcherServlet() {
         return new DispatcherServlet();
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(HelloApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(HelloApplication.class, args);
     }
 }
